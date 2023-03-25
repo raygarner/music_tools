0a29c2185cb7364cb7ddc709fb4f6204cbdaf8f4
rng <ray@perfectcast.com>
2023-03-25 15:16:23 +0000

    reworked int

--- int.c
+++ int.c
@@ -1,12 +1,8 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 #include "common.h"
-
-int degree_val(int, int);
-int is_degree_flat(int, int);
 
 int
 degree_val(int d, int m)
@@ -19,18 +15,32 @@ degree_val(int d, int m)
 }
 
 int
-is_degree_flat(int d, int m)
+correct_alteration(int d, int m, int alter)
 {
-	return degree_val(d, m) < degree_val(d, IONIAN) ? TRUE : FALSE;
+	int x = degree_val(d, m) - degree_val(d, IONIAN);
+	
+	switch (alter) {
+	case NATURAL:
+		return x == alter;
+	case FLAT:
+		return x < 0;
+	case SHARP:
+		return x > 0;
+	}
+	return ERROR;
 }
 
 int
 main(int argc, char *argv[])
 {
-	int m, n, i, len,
-	    key_field[TONES][DEGREES];
+	int m, n, i, len, key_field[TONES][DEGREES], alter, d;
 	char tmp;
-	
+
+	if (argc < 2) {
+		printf("Please pass alterations to major scale degrees\n");
+		printf("eg: int -3-7n6 (flat 3rd, flat 7th, natural 6th\n");
+		return 1;
+	}
 	if (argc > 2 && argv[2][0] == ALL_KEYS) {
 		init_key_field(key_field, 0);
 	} else {
@@ -38,16 +48,22 @@ main(int argc, char *argv[])
 		read_key_list(key_field, 0);
 	}
 	len = strlen(argv[1]);
-	for (i = 0; i < len; i++) { /* for each flattened degree */
+	if (len % 2) {
+		printf("Please pass alterations to major scale degrees\n");
+		printf("eg: int -3-7n6 (flat 3rd, flat 7th, natural 6th)\n");
+		return 1;
+	}
+	for (i = 0; i < len-1; i+=2) { /* for each flattened degree */
 		for (m = 0; m < DEGREES; m++) { /* for each mode */
-			tmp = argv[1][i];
-			if (is_degree_flat(atoi(&tmp), m)) {
-				/* increment all keys of this mode */
+			alter = read_accidental(argv[1][i]);
+			tmp = argv[1][i+1];
+			d = atoi(&tmp);
+			if (correct_alteration(d, m, alter)) {
 				for (n = 0; n < TONES; n++)
 					key_field[n][m]++;
 			}
 		}
 	}
-	print_matching_keys(key_field, len);
+	print_matching_keys(key_field, len/2);
 	return 0;
 }
